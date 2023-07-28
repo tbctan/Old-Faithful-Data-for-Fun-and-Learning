6e8f2c376db22e785c6b8b3a6dc3375f553af60b
tbctan <beatrizchoitan@gmail.com>
2023-07-28 17:03:16 +0900

    first commit

--- Untitled1.py
+++ Untitled1.py
@@ -1,44 +1,36 @@
-#!/usr/bin/env python
-# coding: utf-8
 
-# In[19]:
-
-
-import numpy as np
 import pandas as pd
-import math 
 import matplotlib.pyplot as plt
-
 from sklearn import datasets,mixture
 from sklearn.mixture import GaussianMixture
 from sklearn.cluster import KMeans
 
+
+df=[]
 df = pd.read_csv('data.tsv',sep="\t")
 print(df)
 
 plt.ylabel('Waiting time')
 plt.xlabel('Eruption time')
 plt.plot(df['eruptions'],df['waiting'],linestyle='',marker='o')
-
+print(df)
 
 #Resources: https://scikit-learn.org/stable/modules/generated/sklearn.mixture.GaussianMixture.html#sklearn.mixture.GaussianMixture.fit
 #https://scikit-learn.org/stable/modules/generated/sklearn.cluster.KMeans.html
 
 
-# In[64]:
 
 
-k_iter=5
-g_iter=5
+iter=25
 
 
-GM = GaussianMixture(n_components=2, init_params='random',max_iter=g_iter)
-KM = KMeans(n_clusters=2,max_iter=k_iter)
-
+GM = GaussianMixture(n_components=2, init_params='random',max_iter=iter)
+KM = KMeans(n_clusters=2,init='random',max_iter=iter)
+dfn=pd.DataFrame()
 kmfit = KM.fit(df)
 
 kmcenters=kmfit.cluster_centers_
-df['kmlabels']=kmfit.labels_
+dfn['kmlabels']=kmfit.labels_
 
 print('K-Means')
 #print(kmfit.labels_)
@@ -48,7 +40,7 @@ print('')
 
 gmfit = GM.fit(df)
 gmcenters = gmfit.means_
-df['gmlabels']=gmfit.predict(df)
+dfn['gmlabels']=gmfit.predict(df)
 #df['gmlabels'] = gmfit.labels_
 #gmcenters=gmfit.cluster_centers_
 
@@ -63,28 +55,18 @@ plt.rcParams["figure.figsize"] = (15,18)
 plt.subplot(2,1,1)
 for i in range(0,len(df)):
     plt.plot(df['eruptions'][i],df['waiting'][i],linestyle='',
-             marker='o',color=colors[df['kmlabels'][i]])
-#plt.plot(kmcenters[0],kmcenters[1],linestyle='',marker='+',color='red')
+             marker='o',color=colors[dfn['kmlabels'][i]])
+plt.plot(kmcenters[0,0],kmcenters[0,1],linestyle='',marker='x',color='red',markersize=16)
+plt.plot(kmcenters[1,0],kmcenters[1,1],linestyle='',marker='x',color='red',markersize=16)
 plt.ylabel('Waiting time')
-plt.xlabel('Eruption time, K-Means clustering, %s iter' % k_iter)
+plt.xlabel('Eruption time, K-Means clustering, %s iter' % iter)
 
 plt.subplot(2,1,2)
 for i in range(0,len(df)):
     plt.plot(df['eruptions'][i],df['waiting'][i],linestyle='',
-             marker='o',color=colors[df['gmlabels'][i]])
-#plt.plot(kmcenters[0],kmcenters[1],linestyle='',marker='+',color='red')
+             marker='o',color=colors[dfn['gmlabels'][i]])
+
+plt.plot(gmcenters[0,0],gmcenters[0,1],linestyle='',marker='x',color='red',markersize=16)
+plt.plot(gmcenters[1,0],gmcenters[1,1],linestyle='',marker='x',color='red',markersize=16)
 plt.ylabel('Waiting time')
-plt.xlabel('Eruption time, GM/EM clustering, %s iter' % g_iter)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
+plt.xlabel('Eruption time, GM/EM clustering, %s iter' % iter)
